5e4b494151bce136fe189d1aaaac8e3aed23b861
Roxor <junyperez97@gmail.com>
2021-11-10 11:55:51 -0500

    se elimina algunos comentarios y se imprime el semiperimetro

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 var lado1 = prompt("Ingrese el valor del primer lado");
 var lado2 = prompt("Ingrese el valor del segundo lado");
 var lado3 = prompt("Ingrese el valor del tercer lado");
-//se calcula el perimetro
+
 var perimetro = lado1 + lado2 + lado3;
-//se calcula el semiperimetro
+
 var semiperimetro = perimetro/2;
-//se calcula el area
+console.log(semiperimetro);
 var area = Math.sqrt(semiperimetro*(semiperimetro-lado1)*(semiperimetro-lado2)*(semiperimetro-lado3));
-//se muestra el resultado
+
 alert("El perimetro del triangulo es " + perimetro + " y el area es " + area);
 
